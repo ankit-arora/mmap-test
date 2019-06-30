c2bc3892caae9e0ffe354ec97f461fa9e4df9f76
ankit <ankit.j.arora@gmail.com>
2019-06-30 09:18:27 +0530

    updated to not create file everytime

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module mmap-test
+
+go 1.12
+
+require github.com/pkg/profile v1.3.0
--- main.go
+++ main.go
@@ -8,14 +8,24 @@ import (
 )
 
 func main() {
-	const n = 100
+	//defer profile.Start(profile.MemProfile, profile.MemProfileRate(1)).Stop()
+	const n = 2
 	t := int(unsafe.Sizeof(0)) * n
 
-	map_file, err := os.Create("/tmp/test.dat")
+	fmt.Println(t)
+
+	//map_file, err := os.Create("/tmp/test.dat")
+	//if err != nil {
+	//	fmt.Println(err)
+	//	os.Exit(1)
+	//}
+
+	map_file, err := os.OpenFile("/tmp/test.dat", os.O_RDWR|os.O_CREATE, 0)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
 	_, err = map_file.Seek(int64(t-1), 0)
 	if err != nil {
 		fmt.Println(err)
@@ -34,9 +44,9 @@ func main() {
 	}
 	map_array := (*[n]int)(unsafe.Pointer(&mmap[0]))
 
-	for i := 0; i < n; i++ {
-		map_array[i] = i * i
-	}
+	//for i := 0; i < n; i++ {
+	//	map_array[i] = i * i
+	//}
 
 	fmt.Println(*map_array)
 
